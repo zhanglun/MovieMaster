359ac59874e5bf10e71aaa2454e4fff782ce901c
zhanglun <zhanglun1410@gmail.com>
2016-08-04 17:09:31 +0800

    gulp启动项目，webpack和electron的重启合在一起

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,39 @@
 var gulp = require('gulp');
+var gutil = require('gulp-util');
+var webpack = require('webpack');
+var webpackConfig = require('./webpack.config.js');
 var electron = require('electron-connect').server.create();
 
-gulp.task('default', function () {
-  electron.start();
-  gulp.watch(['./app/src/main.js', './app/src/main/**/*.js'], electron.restart);
-  gulp.watch(['./app/src/**/*.{html,js,css}'], electron.reload);
+// 开发
+var webpackConfigDev = Object.create(webpackConfig);
+webpackConfigDev.devtool = 'eval-source-map';
+webpackConfigDev.debug = true;
+// create a single instance of the compiler to allow caching
+var devCompiler = webpack(webpackConfigDev);
+
+gulp.task('webpack:build-dev', function () {
+  devCompiler.run(function (err, status) {
+    if (err) {
+      throw new gutil.PluginError('webpack:build-dev', err);
+    }
+    gutil.log('[webpack:build-dev]', status.toString({
+      colors: true
+    }));
+  });
 });
+
+gulp.task('watch:electron', function () {
+  electron.start();
+  console.log('---->electron starting');
+  gulp.watch(['./app/src/main.js', './app/src/main/**/*.js'], electron.restart);
+  gulp.watch(['./app/dist/**/*.{html,js,css}'], electron.reload);
+});
+
+gulp.task('watch:webpack-dev', function () {
+  console.log('---->react building');
+  gulp.watch(['./app/src/**/*.{html,js,css}'], ['webpack:build-dev']);
+});
+
+gulp.task('watch', ['watch:webpack-dev', 'watch:electron']);
+gulp.task('dev', ['webpack:build-dev', 'watch']);
+gulp.task('default', ['dev']);
