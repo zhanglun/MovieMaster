88c04f1a54f082c3e5602d859220f250cfb4c44d
zhanglun <zhanglun1410@gmail.com>
2016-08-21 16:54:44 +0800

    update ui

--- app/src/components/movieItem/index.js
+++ app/src/components/movieItem/index.js
@@ -14,7 +14,39 @@ class MovieItem extends Component {
           <img className="movie-card__poster-img" src={movie.poster}/>
         </div>
         <div className="movie-card__info">
-          <div className="movie-card__title">{movie.name}</div>
+          <div className="movie-card__title">{movie.name} ({movie.region})</div>
+          <div className="movie-card__info-item">
+            <span className="movie-card__info-head">
+              导演:
+            </span>
+            <span className="movie-card__info-content">
+              {movie.director}
+            </span>
+          </div>
+          <div className="movie-card__info-item">
+            <span className="movie-card__info-head">
+            类型:
+            </span>
+            <span className="movie-card__info-content">
+              {movie.type}
+            </span>
+          </div>
+          <div className="movie-card__info-item">
+            <span className="movie-card__info-head">
+            别名:
+            </span>
+            <span className="movie-card__info-content">
+              {movie.alias}
+            </span>
+          </div>
+          <div className="movie-card__info-item">
+            <span className="movie-card__info-head">
+            简介:
+            </span>
+            <span className="movie-card__info-content">
+            {movie.synopsis.slice(0, 56) + '...'}
+            </span>
+          </div>
         </div>
       </div>
     )
