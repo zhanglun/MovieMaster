e70e0e215503f3c7e9cde548d0f63de0eb328839
zhanglun <zhanglun1410@gmail.com>
2016-09-15 17:27:02 +0800

    添加侧边按钮

--- app/src/components/sidebar/index.js
+++ app/src/components/sidebar/index.js
@@ -2,18 +2,18 @@ import './index.less';
 import React, { Component } from 'react';
 
 class SideBar extends Component {
-  constructor(props) {
+  constructor (props) {
     super(props);
   }
 
 
-  render() {
+  render () {
     return (
       <div className="sidebar">
         <div className="sidebar-header">
           <div className="sidebar-setting__item" onClick={this.showFolders}>
             <span className="material-icons">folder</span>
-            添加文件夹
+            添加
           </div>
           <div className="sidebar-setting__item">
             <span className="material-icons">settings</span>
@@ -21,7 +21,23 @@ class SideBar extends Component {
           </div>
         </div>
         <div className="sidebar-body">
-
+          <div className="side-menu__title">
+            分类
+          </div>
+          <div className="side-menu">
+            <div className="side-menu__item">
+              <span className="material-icons">movie</span>
+              全部
+            </div>
+            <div className="side-menu__item">
+              <span className="material-icons">movie</span>
+              电影
+            </div>
+            <div className="side-menu__item">
+              <span className="material-icons">movie</span>
+              剧集
+            </div>
+          </div>
         </div>
         <div className="sidebar-footer">
 
