41afb14d6054940d9ef125f38355f91736a86e8a
zhanglun <zhanglun1410@gmail.com>
2016-12-10 23:38:35 +0800

    add toolbar for head

--- app/client/components/Header.js
+++ app/client/components/Header.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { hashHistory, Link } from 'react-router';
 
 class MainTop extends Component {
-  constructor (props) {
+  constructor(props) {
     super(props);
     this.state = {
       openLeftDrawer: false,
@@ -10,81 +10,90 @@ class MainTop extends Component {
     };
   }
 
-  toggleLeftDrawer () {
+  toggleLeftDrawer() {
     this.setState({ openLeftDrawer: !this.state.openLeftDrawer });
   }
 
-  handleClose () {
+  handleClose() {
     this.setState({ openLeftDrawer: false });
   }
 
-  render () {
+  renderHeaderNavBar() {
     return (
-        // <div className="main-topbar">
-        //   <div className="appbar">
-        //     <div className="appbar-drawerbtn">
-        //     </div>
-        //     <div className="search-box">
-        //       <input type="text" className="search-box__input" placeholder="Search"/>
-        //     </div>
-        //   </div>
-        //   <div className="action-bar">
-        //     <div className="action-bar__item">
-        //       <span className="action-bar__link">Genres
-        //         {/*<span className='material-icons'>expand_more</span>*/}
-        //       </span>
-        //     </div>
-        //     <div className="action-bar__spacer"></div>
-        //     <div className="action-bar__item">
-        //       <Link className="action-bar__link" activeClassName="action-bar__link--active"
-        //             to={'/'}>Home</Link>
-        //     </div>
-        //     <div className="action-bar__item">
-        //       <Link className="action-bar__link">Recent</Link>
-        //     </div>
-        //   </div>
-        // </div>
-      <nav className="navbar navbar-default">
+      <nav className="navbar navbar-default navbar-inverse">
         <div className="container-fluid">
           <div className="navbar-header">
-            <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-              <span className="sr-only">Toggle navigation</span>
-              <span className="icon-bar"></span>
-              <span className="icon-bar"></span>
-              <span className="icon-bar"></span>
-            </button>
-            <a className="navbar-brand" href="#">Brand</a>
+            <div className="btn-toolbar" role="toolbar">
+              <div className="btn-group" role="group">
+                <span type="button" className="btn btn-sm btn-primary navbar-btn material-icons">
+                  chevron_left</span>
+                <span type="button" className="btn btn-sm btn-primary navbar-btn material-icons">
+                  chevron_right
+                </span>
+              </div>
+            </div>
           </div>
 
-          <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            <ul className="nav navbar-nav">
-              <li className="active"><a href="#">Link <span className="sr-only">(current)</span></a></li>
-              <li><a href="#">Link</a></li>
-              <li className="dropdown">
-                <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Dropdown <span className="caret"></span></a>
-                <ul className="dropdown-menu" role="menu">
-                  <li><a href="#">Action</a></li>
-                  <li><a href="#">Another action</a></li>
-                  <li><a href="#">Something else here</a></li>
-                  <li className="divider"></li>
-                  <li><a href="#">Separated link</a></li>
-                  <li className="divider"></li>
-                  <li><a href="#">One more separated link</a></li>
-                </ul>
-              </li>
-            </ul>
-            <form className="navbar-form navbar-left" role="search">
-              <div className="form-group">
-                <input type="text" className="form-control" placeholder="Search" />
-              </div>
-              <button type="submit" className="btn btn-default">Submit</button>
-            </form>
-            <ul className="nav navbar-nav navbar-right">
-              <li><a href="#">Link</a></li>
-            </ul>
-          </div>
+          {/*<div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">*/}
+            {/*<ul className="nav navbar-nav">*/}
+              {/*<li className="active"><a href="#">Link <span className="sr-only">(current)</span></a>*/}
+              {/*</li>*/}
+              {/*<li><a href="#">Link</a></li>*/}
+              {/*<li className="dropdown">*/}
+                {/*<a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button"*/}
+                   {/*aria-expanded="false">Dropdown <span className="caret"></span></a>*/}
+                {/*<ul className="dropdown-menu" role="menu">*/}
+                  {/*<li><a href="#">Action</a></li>*/}
+                  {/*<li><a href="#">Another action</a></li>*/}
+                  {/*<li><a href="#">Something else here</a></li>*/}
+                  {/*<li className="divider"></li>*/}
+                  {/*<li><a href="#">Separated link</a></li>*/}
+                  {/*<li className="divider"></li>*/}
+                  {/*<li><a href="#">One more separated link</a></li>*/}
+                {/*</ul>*/}
+              {/*</li>*/}
+            {/*</ul>*/}
+            {/*<ul className="nav navbar-nav navbar-right">*/}
+              {/*<li><a href="#">Link</a></li>*/}
+            {/*</ul>*/}
+          {/*</div>*/}
         </div>
       </nav>
+    )
+  }
+
+  renderCustomNavBar() {
+    return (
+      <div className="main-topbar">
+        <div className="appbar">
+          <div className="appbar-drawerbtn">
+          </div>
+          <div className="search-box">
+            <input type="text" className="search-box__input" placeholder="Search"/>
+          </div>
+        </div>
+        <div className="action-bar">
+          <div className="action-bar__item">
+            <span className="action-bar__link">Genres
+              {/*<span className='material-icons'>expand_more</span>*/}
+            </span>
+          </div>
+          <div className="action-bar__spacer"></div>
+          <div className="action-bar__item">
+            <Link className="action-bar__link" activeClassName="action-bar__link--active"
+                  to={'/'}>Home</Link>
+          </div>
+          <div className="action-bar__item">
+            <Link className="action-bar__link">Recent</Link>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  render() {
+    return (
+      this.renderHeaderNavBar()
     )
   }
 }
--- app/client/containers/App.js
+++ app/client/containers/App.js
@@ -5,37 +5,43 @@ import HeadComponent from '../components/Header';
 
 
 class App extends Component {
-  constructor (props) {
+  constructor(props) {
     super(props);
     this.initData = this.initData.bind(this);
   }
 
-  initData () {
+  initData() {
     // 备用代码。打开文件夹的简单逻辑
   }
 
-  componentWillMount () {
+  componentWillMount() {
   }
 
-  componentDidMount () {
+  componentDidMount() {
     this.initData();
     // dispatch(fetchMoviesInfo());
   }
 
-  componentWillReceiveProps (nextprops) {
+  componentWillReceiveProps(nextprops) {
   }
 
 
-  render () {
+  render() {
     return (
+      <div className="container-full">
         <div className="app">
           <div className="main">
             <HeadComponent />
+            <div className="row">
+              <div className="col-md-2 no-padding">.col-md-1</div>
+              <div className="col-md-10">.col-md-1</div>
+            </div>
             <div className="main-body">
               {this.props.children}
             </div>
           </div>
         </div>
+      </div>
     );
   }
 }
