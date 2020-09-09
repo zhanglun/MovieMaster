7bfa393070d611c0ad0e3b649b51c61d28013218
zhanglun <zhanglun1410@gmail.com>
2020-09-09 21:31:39 +0800

    fix: fix dependencies

--- app/client/index.tsx
+++ app/client/index.tsx
@@ -4,7 +4,9 @@ import * as ReactDOM from "react-dom";
 import { Hello } from './components/app';
 
 ReactDOM.render(
-  // <Hello compiler="TypeScript" framework="React" />,
-  <h1>Hello</h1>,
+  <div>
+    <Hello compiler="TypeScript" framework="React" />,
+    <h1>Hello</h1>
+  </div>,
   document.getElementById("app")
 );
